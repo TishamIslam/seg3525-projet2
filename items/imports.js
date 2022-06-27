5c90472a99173be9441b222d7b53cecc202eb182
tishamIslam <tisham.islam@gmail.com>
2022-06-27 18:25:51 -0400

    Ajout de pages de composants dynamiques partie 1

---
+++ items/imports.js
@@ -0,0 +1,54 @@
+import cpu1 from "./cpu/cpu1.jpg"
+import cpu2 from "./cpu/cpu2.jpg"
+import cpu3 from "./cpu/cpu3.jpg"
+import cpu4 from "./cpu/cpu4.jpg"
+import cpu5 from "./cpu/cpu5.jpg"
+
+import gpu1 from "./gpu/gpu1.jpg"
+import gpu2 from "./gpu/gpu2.jpg"
+import gpu3 from "./gpu/gpu3.jpg"
+import gpu4 from "./gpu/gpu4.jpg"
+import gpu5 from "./gpu/gpu5.jpg"
+
+import motherboard1 from "./motherboard/motherboard1.jpg"
+import motherboard2 from "./motherboard/motherboard2.jpg"
+import motherboard3 from "./motherboard/motherboard3.jpg"
+import motherboard4 from "./motherboard/motherboard4.jpg"
+import motherboard5 from "./motherboard/motherboard5.jpg"
+
+import power1 from "./power/power1.jpg"
+import power2 from "./power/power2.jpg"
+import power3 from "./power/power3.jpg"
+import power4 from "./power/power4.jpg"
+import power5 from "./power/power5.jpg"
+
+export const computerComponents = {
+    gpu: [
+        {img: gpu1, alt: "GPU 1", name: "GPU 1", price: "Prix: $2500"},
+        {img: gpu2, alt: "GPU 2", name: "GPU 2", price: "Prix: $1500"},
+        {img: gpu3, alt: "GPU 3", name: "GPU 3", price: "Prix: $3000"},
+        {img: gpu4, alt: "GPU 4", name: "GPU 4", price: "Prix: $1750"},
+        {img: gpu5, alt: "GPU 5", name: "GPU 5", price: "Prix: $1000"},
+    ], 
+    cpu: [
+        {img: cpu1, alt: "CPU 1", name: "CPU 1", price: "Prix: $2500"},
+        {img: cpu2, alt: "CPU 2", name: "CPU 2", price: "Prix: $1500"},
+        {img: cpu3, alt: "CPU 3", name: "CPU 3", price: "Prix: $3000"},
+        {img: cpu4, alt: "CPU 4", name: "CPU 4", price: "Prix: $1750"},
+        {img: cpu5, alt: "CPU 5", name: "CPU 5", price: "Prix: $1000"},
+    ], 
+    motherboard: [
+        {img: motherboard1, alt: "Carte-mère 1", name: "Carte-mère 1", price: "Prix: $200"},
+        {img: motherboard2, alt: "Carte-mère 2", name: "Carte-mère 2", price: "Prix: $250"},
+        {img: motherboard3, alt: "Carte-mère 3", name: "Carte-mère 3", price: "Prix: $300"},
+        {img: motherboard4, alt: "Carte-mère 4", name: "Carte-mère 4", price: "Prix: $150"},
+        {img: motherboard5, alt: "Carte-mère 5", name: "Carte-mère 5", price: "Prix: $175"},
+    ],
+    power: [
+        {img: power1, alt: "Alimentation électrique 1", name: "Alimentation électrique 1", price: "Prix: $100"},
+        {img: power2, alt: "Alimentation électrique 2", name: "Alimentation électrique 2", price: "Prix: $50"},
+        {img: power3, alt: "Alimentation électrique 3", name: "Alimentation électrique 3", price: "Prix: $60"},
+        {img: power4, alt: "Alimentation électrique 4", name: "Alimentation électrique 4", price: "Prix: $75"},
+        {img: power5, alt: "Alimentation électrique 5", name: "Alimentation électrique 5", price: "Prix: $125"},
+    ],
+}
