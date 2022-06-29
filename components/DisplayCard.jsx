691f9267278e2722db81292a75741b3bf17bb103
tishamIslam <tisham.islam@gmail.com>
2022-06-29 16:23:23 -0400

    CarouselCard renamed to more generic DisplayCard

---
+++ components/DisplayCard.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function DisplayCard(props) {
+    // const aspectRatio = (props?.aspectRatio ?? "") === "16/9" ? "next-image-ratio-16-9" : props.aspectRatio
+    const card = (
+        <div className="space-y-0 p-0 my-4 shadow-xl">
+            <div className={"h-min rounded-t-md align-middle leading-none next-image-block "}>
+                <Image src={props.image} alt={props.imageAlt} placeholder="blur"
+                objectFit={props?.objectFit ?? "cover"} width={props?.imgWidth} height={props?.imgHeight} 
+                />
+            </div>
+            <div className="p-2 text-white rounded-b-md bg-green">
+                {props.text}
+            </div>
+        </div>
+    )
+
+    return (
+        <div className={"w-full"}>
+            {props?.URL ?? false ? <Link href={props.URL}>
+                <a className="no-underline">
+                    {card}
+                </a>
+            </Link> : card}
+        </div>
+    )
+}
