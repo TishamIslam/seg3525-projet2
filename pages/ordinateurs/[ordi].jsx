c4e9bac1cb5b8ab19dee47c49416b6c8129a3dc2
tishamIslam <tisham.islam@gmail.com>
2022-06-28 22:08:39 -0400

    Ajout de section commentaires

--- pages/ordinateurs/[ordi].jsx
+++ pages/ordinateurs/[ordi].jsx
@@ -1,15 +1,16 @@
 import { useRouter } from "next/router"
 
 import CarouselCard from "../../components/CarouselCard"
+import Comment from "../../components/Comment"
 import { computers } from "../../items/imports"
 
-export default function Ordinateur() {    
+export default function Ordinateur() {
     const router = useRouter()
     const { ordi } = router.query
 
     const cardText = (
         <div className="p-2">
-            <h3 className="text-lg">Ordinateur A</h3>
+            <h2 className="text-xl">Ordinateur A</h2>
             <p>CPU: Details</p>
             <p>GPU: Details</p>
             <p>RAM: Details</p>
@@ -27,9 +28,26 @@ export default function Ordinateur() {
 
     return (
         <>
-        <div className="p-6 ">
-            {renderComputer()}
-        </div>
+            <h1>Détails de Ordinateur A</h1>
+            <div className="my-4 max-w-xl mx-auto">
+                {renderComputer()}
+            </div>
+
+            <h2 className="mt-8 text-center "> Section de commentaires</h2>
+            <div className="py-2 md:grid md:grid-cols-2 md:gap-8 mx-auto w-fit">
+                <Comment name="Commenter 1" />
+                <Comment name="Commenter 2" />
+                <Comment />
+                <Comment name="Commenter 3" />
+                <Comment name="Commenter 4" />
+                <Comment />
+            </div>
+
+            <h3 className="text-lg my-2">Ajouter un commentaire</h3>
+            <form >
+                <input name="comment-name" className="border border-green my-2 rounded p-2 placeholder:text-gray-400" type="text" placeholder="Nom" />
+                <textarea className="border border-green block rounded p-2 my-2 w-full max-w-md" name="comment-text" rows="4" placeholder="J'aime le build! Blah Blah Blah" />
+            </form>
         </>
     )
 }
