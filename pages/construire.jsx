e7fbea3945d9990ee2045db1e7861bc9337053d4
tishamIslam <tisham.islam@gmail.com>
2022-06-29 20:53:20 -0400

    Revamped build page

--- pages/construire.jsx
+++ pages/construire.jsx
@@ -9,66 +9,93 @@ export default function BuildComputer() {
     const chosenHDD = computerComponents['storage'][0]
     const chosenNVMe = computerComponents['storage'][6]
     return (
-        <div className="space-y-2">
-            <h1 className="text-xl">Créez un Ordinateur</h1>
+        <div className="space-y-6">
+            <h1>Créez un Ordinateur</h1>
             <span>* indique un composante requis</span>
 
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>CPU Choisi: </span>
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">CPU Choisi: </span>
+                </div>
+                <div className="p-2">
+                    <ItemCard image={AMDRyzen9} imageAlt="AMD Ryzen 9 CPU" button={true}
+                        text={(<div><p>AMD Ryzen 9 5900x</p><p>Prix: 2000 $</p></div>)} />
+                </div>
             </div>
-            <ItemCard image={AMDRyzen9} imageAlt="AMD Ryzen 9 CPU" button={true}
-            text={(<div><p>AMD Ryzen 9 5900x</p><p>Prix: 2000 $</p></div>)} />
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter un carte-mère *</span>
-                <ActionButton plus={true} link="/composants/motherboard" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter un carte-mère *</span>
+                    <ActionButton plus={true} link="/composants/motherboard" />
+                </div>
             </div>
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Composants mémoire choisis:</span>
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Composants mémoire choisis (Maximum varie selon le boîtier):</span>
+                    <ActionButton plus={true} link="/composants/storage" />
+                </div>
+                <div className="space-y-6 p-2 mt-2">
+                    <ItemCard image={chosenHDD.img} imageAlt={chosenHDD.alt} button={true}
+                        text={(<div><p>{chosenHDD.name}</p><p>Prix: {chosenHDD.price} $</p></div>)} />
+                    <ItemCard image={chosenNVMe.img} imageAlt={chosenNVMe.alt} button={true}
+                        text={(<div><p>{chosenNVMe.name}</p><p>Prix: {chosenNVMe.price} $</p></div>)} />
+                </div>
             </div>
-            <ItemCard image={chosenHDD.img} imageAlt={chosenHDD.alt} button={true}
-            text={(<div><p>{chosenHDD.name}</p><p>Prix: {chosenHDD.price} $</p></div>)} />
-            <ItemCard image={chosenNVMe.img} imageAlt={chosenNVMe.alt} button={true}
-            text={(<div><p>{chosenNVMe.name}</p><p>Prix: {chosenNVMe.price} $</p></div>)} />
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter du RAM *</span>
-                <ActionButton plus={true} link="/composants/ram" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter du RAM *</span>
+                    <ActionButton plus={true} link="/composants/ram" />
+                </div>
             </div>
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter un ventilateur</span>
-                <ActionButton plus={true} link="/composants/fan" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter un ventilateur</span>
+                    <ActionButton plus={true} link="/composants/fan" />
+                </div>
             </div>
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter un carte graphique *</span>
-                <ActionButton plus={true} link="/composants/gpu" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter un carte graphique *</span>
+                    <ActionButton plus={true} link="/composants/gpu" />
+                </div>
             </div>
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter un boîtier * </span>
-                <ActionButton plus={true} link="/composants/case" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter un boîtier * </span>
+                    <ActionButton plus={true} link="/composants/case" />
+                </div>
             </div>
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter l'alimentation électrique *</span>
-                <ActionButton plus={true} link="/composants/power" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter l'alimentation électrique *</span>
+                    <ActionButton plus={true} link="/composants/power" />
+                </div>
             </div>
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Système d'exploitation choisi:</span>
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Système d'exploitation choisi:</span>
+                </div>
+                <div className="p-2">
+                    <ItemCard image={chosenOS.img} imageAlt={chosenOS.alt} button={true}
+                        text={(<div><p>{chosenOS.name}</p><p>Prix: {chosenOS.price} $</p></div>)} />
+                </div>
             </div>
-            <ItemCard image={chosenOS.img} imageAlt={chosenOS.alt} button={true}
-            text={(<div><p>{chosenOS.name}</p><p>Prix: {chosenOS.price} $</p></div>)} />
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span>Ajouter des accessoires</span>
-                <ActionButton plus={true} link="/composants/accessory" />
+            <div className="bg-mint p-2 py-3 rounded-lg">
+                <div className="p-2 flex justify-between items-center">
+                    <span className="text-lg md:text-xl">Ajouter des accessoires</span>
+                    <ActionButton plus={true} link="/composants/accessory" />
+                </div>
             </div>
 
             {/* Video game analysis section */}
-            <hr className="border-2 border-green" />
-            <div className="py-4">    
+            <hr className="border border-green" />
+            <div className="py-8">
                 <h2>Erreur: Tu n'a pas choisi assez de composants pour voir un analyse de jeux vidéos</h2>
             </div>
 
             {/* Buying section */}
-            <hr className="border-2 border-green" />
-            <div className="p-2 my-2 flex justify-between items-center">
-                <span className="text-lg">Coût totale: 2315 $</span>
+            <hr className="border border-green" />
+            <div className="p-2 py-8 grid grid-cols-1 gap-2 md:flex justify-between items-center">
+                <span className="text-xl text-center md:text-left">Coût totale: 2315 $</span>
                 <button className="p-2 rounded-md bg-green hover:bg-green-light text-white">
                     Achetez maintenant!
                 </button>
